25401915d02d6dcdaf52d631f66993dc9c6dac72
Jan Kuris <jan.kuris@gmail.com>
2024-04-19 22:20:31 +0200

    final commit

--- main.py
+++ main.py
@@ -1,44 +1,55 @@
 import random
+import os 
 from game_data import data_list
 from art import logo, vs 
 
+def clear_screen():
+    os.system('cls' if os.name == 'nt' else 'clear') 
 
-def get_random_dict(data_list):
-    random_result = random.choice(data_list)
-    return random_result  
-# def compare():
-#     random_result_A = get_random_dict(data_list)
-#     random_result_B = get_random_dict(data_list)
-#     compare_value_A = random_result_A['follower_count']
-#     compare_value_B = random_result_B['follower_count']
-#     return random_result_A, random_result_B, compare_value_A, compare_value_B
+def get_random_account():
+    return random.choice(data_list)
 
-def game():
-    attempt = True 
-    score = 0
-    random_result_A = get_random_dict(data_list)
-    random_result_B = get_random_dict(data_list)
-    while random_result_A == random_result_B:
-        random_result_B = get_random_dict(data_list)
-    compare_value_A = random_result_A['follower_count']
-    compare_value_B = random_result_B['follower_count']
-    def listing():
-        random_result_A = get_random_dict(data_list)
-        random_result_B = get_random_dict(data_list)
-        compare_value_A = random_result_A['follower_count']
-        compare_value_B = random_result_B['follower_count']
-        print(f"Compare A: {compare_value_A} {random_result_A['name']}, {random_result_A['description']} from {random_result_A['country']}")
-        print(vs)
-        print(f"Against B:{compare_value_B} {random_result_B['name']}, {random_result_B['description']} from {random_result_B['country']}")
+def format_account_info(acount):
+    name = acount['name']
+    description = acount['description']
+    city = acount['country']
+    return f"{name} {description} from {city}"
+
+def check_answer(guess, a_account, b_account,):
+    if a_account > b_account:
+        return guess == "a"
+    else:
+        return guess == "b"
     
-    while attempt:
-        listing()
-        user_input = input("Who has more followers? Type A/B: ").lower()    
-        if (compare_value_A > compare_value_B and user_input == 'a') or (compare_value_A < compare_value_B and user_input == "b"):
-            score += 1
-            print("you'r right")
-        else:
-            print(f"Sorry that's wrong. Your score is {score}")
-        print (score)
+def game():
+    # print(logo)
+    score = 0
+    game_should_continue = True
+    a_account = get_random_account()
+    b_account = get_random_account()
+    
+    while game_should_continue:
+        a_account = b_account
+        b_account = get_random_account()
 
+        while a_account == b_account:
+            b_account = get_random_account()
+        
+        print(f"Compare A: {format_account_info(a_account)}")
+        print(vs)
+        print(f"Compare B: {format_account_info(b_account)}")
+
+        guess = input("Who has more followers? Type 'A' or 'B': ").lower()
+        a_follower_numb = a_account['follower_count']
+        b_follower_numb = b_account['follower_count']
+        is_correct = check_answer(guess, a_follower_numb, b_follower_numb)
+
+        clear_screen()
+        print(logo)
+        if is_correct:
+            score += 1 
+            print(f"You are right! Current score: {score} ")
+        else:
+            game_should_continue = False
+            print(f"Sorry, that's wrong. Final score: {score}")
 game()
